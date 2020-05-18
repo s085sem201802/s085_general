5f32a8e3760f230b63d0f0f270bf7833f3d1349f
Ubuntu <ubuntu@ip-172-31-83-216.ec2.internal>
2020-05-18 18:40:40 +0000

    Test of exercices at AWS EC2

--- 04-22/client.py
+++ 04-22/client.py
@@ -2,11 +2,18 @@ import zmq
 from constCS import * #-
 context = zmq.Context()
 
-p1 = "tcp://"+ HOST +":"+ PORT1 # how and where to connect
+p1 = "tcp://172.31.93.112:2345" # how and where to connect
 s  = context.socket(zmq.REQ)    # create request socket
 
 s.connect(p1)                   # block until connected
-s.send("Hello world")           # send message
+s.send("Hello world p1")           # send message
 message = s.recv()              # block until response
+print message                   # print result
+s.send("Hello world p2")           # send message
+message = s.recv()              # block until response
+print message                   # print result
+s.send("Hello world p3")           # send message
+message = s.recv()              # block until response
+print message                   # print result
 s.send("STOP")                  # tell server to stop
 print message                   # print result
--- 04-23/client.py
+++ 04-23/client.py
@@ -1,12 +1,16 @@
 import zmq
 from constPS import * #-
 
+HOST = "172.31.93.112"
+PORT = "2345"
+
 context = zmq.Context()
 s = context.socket(zmq.SUB)          # create a subscriber socket
 p = "tcp://"+ HOST +":"+ PORT        # how and where to communicate
 s.connect(p)                         # connect to the server
 s.setsockopt(zmq.SUBSCRIBE, "TIME")  # subscribe to TIME messages
 
-for i in range(5):  # Five iterations
+#for i in range(5):  # Five iterations
+while True:
   time = s.recv()   # receive a message 
   print time       
